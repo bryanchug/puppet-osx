5d1e1e0142fc81516739f859882067111a17ebdc
Bryan Chug <bryan.chug@gmail.com>
2014-08-09 18:50:17 +0800

    Added osx::screenshots -- oops added directory ensure

--- manifests/screenshots.pp
+++ manifests/screenshots.pp
@@ -1,5 +1,10 @@
 # Public: Set location for screenshots
 class osx::screenshots($path = "/Users/${::boxen_user}/screenshots") {
+
+  file { $path:
+    ensure => directory
+  }
+  
   boxen::osx_defaults { 'Screenshots default location':
     user   => $::boxen_user,
     ensure => present,
